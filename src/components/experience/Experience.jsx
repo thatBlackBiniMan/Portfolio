7097fa10df3aa90b7ac142b54cb4c0e8fc6455ab
@thatBlackBiniMan <solomonomoruyi66@gmail.com>
2022-11-26 22:48:14 +0100

    Working on Experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,103 @@
 import React from 'react'
 import './experience.css';
-
+import {BsPatchCheckFill} from 'react-icons/bs';
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skills I have</h5>
+       <h2>My Experience</h2>
+  
+     <div className="container experience_container">
+      <div className="experience_frontend">
+       <h3>Medical & Academic Experiences</h3>
+      <div className="experience_content">
+     
+     
+      <article className='experience_details'>
+         <BsPatchCheckFill/>
+         <h4>Clinical Medicine and Basic Surgical Skills</h4>
+        <small className='text-light'>Experienced</small>
+         </article>
+
+     
+     
+     
+        <article className='experience_details'>
+         <BsPatchCheckFill/>
+         <h4>Research Writing</h4>
+        <small className='text-light'>Experienced</small>
+         </article>
+
+         <article className='experience_details'>
+         <BsPatchCheckFill/>
+         <h4>Article Writing</h4>
+        <small className='text-light'>Experienced</small>
+         </article>
+
+     
+
+         <article className='experience_details'>
+         <BsPatchCheckFill/>
+         <h4>Ebooks Writing and Design</h4>
+        <small className='text-light'>Experienced</small>
+         </article>
+
+
+         <article className='experience_details'>
+         <BsPatchCheckFill/>
+         <h4>Clinical Medicine and Basic Surgical Skills</h4>
+        <small className='text-light'>Experienced</small>
+         </article>
+
+      </div>
+      </div>
+     
+      <div className="experience_backend">
+      <div className="experience_content">
+     
+     
+     <article className='experience_details'>
+        <BsPatchCheckFill/>
+        <h4>Clinical Medicine and Basic Surgical Skills</h4>
+       <small className='text-light'>Experienced</small>
+        </article>
+
+    
+    
+    
+       <article className='experience_details'>
+        <BsPatchCheckFill/>
+        <h4>Research Writing</h4>
+       <small className='text-light'>Experienced</small>
+        </article>
+
+        <article className='experience_details'>
+        <BsPatchCheckFill/>
+        <h4>Article Writing</h4>
+       <small className='text-light'>Experienced</small>
+        </article>
+
+    
+
+        <article className='experience_details'>
+        <BsPatchCheckFill/>
+        <h4>Ebooks Writing and Design</h4>
+       <small className='text-light'>Experienced</small>
+        </article>
+
+
+        <article className='experience_details'>
+        <BsPatchCheckFill/>
+        <h4>Clinical Medicine and Basic Surgical Skills</h4>
+       <small className='text-light'>Experienced</small>
+        </article>
+
+     </div>
+
+      </div>
+     
+     </div>
+    </section>
   )
 }
 
