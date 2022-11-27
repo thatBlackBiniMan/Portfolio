ba6899d20bd8a88be8e3c5d23d0ea4ce94b6cb9b
@thatBlackBiniMan <solomonomoruyi66@gmail.com>
2022-11-27 14:34:45 +0100

    Finished Services sectio

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,10 +1,126 @@
 import React from 'react'
 import './services.css';
-
+import {BiCheck} from 'react-icons/bi';
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+    
+    <div className="container services_container">
+      <article className='service'>
+        <div className="service_head">
+        <h3>Medical Services</h3>      
+        </div>
+      <ul className="service_list">
+        <li>
+          <BiCheck className='service_list-icon'/>
+          <p>Basic Surgical Procedures (Cesearean section, Appendicectomy)</p>
+        </li>
+      
+        <li>
+          <BiCheck className='service_list-icon'/>
+          <p>Basic Surgical Procedures (Cesearean section, Appendicectomy)</p>
+        </li>
+      
+        <li>
+          <BiCheck className='service_list-icon'/>
+          <p>Basic Surgical Procedures (Cesearean section, Appendicectomy)</p>
+        </li>
+      
+        <li>
+          <BiCheck className='service_list-icon'/>
+          <p>Basic Surgical Procedures (Cesearean section, Appendicectomy)</p>
+        </li>
+      
+        <li>
+          <BiCheck className='service_list-icon'/>
+          <p>Basic Surgical Procedures (Cesearean section, Appendicectomy)</p>
+        </li> 
+      
+      </ul>
+      </article>
+    
+    
+      <article className='service'>
+        <div className="service_head">
+        <h3>Software Development</h3>      
+        </div>
+      <ul className="service_list">
+        <li>
+          <BiCheck className='service_list-icon'/>
+          <p>Basic Surgical Procedures (Cesearean section, Appendicectomy)</p>
+        </li>
+      
+        <li>
+          <BiCheck className='service_list-icon'/>
+          <p>Basic Surgical Procedures (Cesearean section, Appendicectomy)</p>
+        </li>
+      
+        <li>
+          <BiCheck className='service_list-icon'/>
+          <p>Basic Surgical Procedures (Cesearean section, Appendicectomy)</p>
+        </li>
+      
+        <li>
+          <BiCheck className='service_list-icon'/>
+          <p>Basic Surgical Procedures (Cesearean section, Appendicectomy)</p>
+        </li>
+      
+        <li>
+          <BiCheck className='service_list-icon'/>
+          <p>Basic Surgical Procedures (Cesearean section, Appendicectomy)</p>
+        </li>
+      
+        <li>
+          <BiCheck className='service_list-icon'/>
+          <p>Basic Surgical Procedures (Cesearean section, Appendicectomy)</p>
+        </li>
+      
+      
+      </ul>
+      </article>
+    
+
+      <article className='service'>
+        <div className="service_head">
+        <h3>Research / Writing</h3>      
+        </div>
+      <ul className="service_list">
+        <li>
+          <BiCheck className='service_list-icon'/>
+          <p>Basic Surgical Procedures (Cesearean section, Appendicectomy)</p>
+        </li>
+      
+        <li>
+          <BiCheck className='service_list-icon'/>
+          <p>Basic Surgical Procedures (Cesearean section, Appendicectomy)</p>
+        </li>
+      
+        <li>
+          <BiCheck className='service_list-icon'/>
+          <p>Basic Surgical Procedures (Cesearean section, Appendicectomy)</p>
+        </li>
+      
+        <li>
+          <BiCheck className='service_list-icon'/>
+          <p>Basic Surgical Procedures (Cesearean section, Appendicectomy)</p>
+        </li>
+      
+        <li>
+          <BiCheck className='service_list-icon'/>
+          <p>Basic Surgical Procedures (Cesearean section, Appendicectomy)</p>
+        </li>
+      
+  
+      
+      </ul>
+      </article>
+    
+    
+    </div>
+    </section>
   )
 }
 
