0590a33fdcb0df1b222acbf6c5fc5daba5858b01
@thatBlackBiniMan <solomonomoruyi66@gmail.com>
2022-11-27 21:14:36 +0100

    Finished Contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,51 @@
 import React from 'react'
 import './contact.css';
-
-
+import {TfiEmail} from 'react-icons/tfi';
+import {FaFacebookMessenger} from 'react-icons/fa';
+import {BsWhatsapp} from 'react-icons/bs';
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Feel Free To</h5>
+      <h2>Contact Me</h2>
+ 
+    <div className="container contact_container">
+      <div className="contact_options">
+        <article className='contact_option'>
+          <TfiEmail className='contact_option-icon'/>
+          <h4>Email</h4>
+          <h5>omoruyiosarugue20@gmail.com</h5>
+          <a href="mailto:omoruyiosarugue20@gmail.com" target="_blank">Send a message</a>
+        </article>
+
+        <article className='contact_option'>
+          <FaFacebookMessenger className='contact_option-icon'/>
+          <h4>Facebook Messenger</h4>
+          <h5>Omoruyi Osarugue</h5>
+          <a href="https://m.me/omoruyi.osarugue" target="_blank">Send a message</a>
+        </article>
+
+        <article className='contact_option'>
+          <BsWhatsapp className='contact_option-icon'/>
+          <h4>Whatsapp</h4>
+          <h5>+2437069093120</h5>
+          <a href="https://api.whatsapp.com/send?phone+2437069093120" target="_blank">Send a message</a>
+        </article>
+
+
+      </div>
+    <form action="">
+      <input type="text" name='name' placeholder='Your full name' required/>
+      <input type="email" name='email' placeholder='Your Email' required/>
+      <textarea name="message" placeholder='Enter Your Message' rows="7" required></textarea>
+      <button type='submit' className='btn btn-primary'>Send Message</button>
+    </form>
+    </div>
+ 
+ 
+ 
+    </section>
   )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,74 @@
 import React from 'react'
 import './testimonials.css';
+import AVTR1 from '../../assets/t1.jpeg';
+import AVTR2 from '../../assets/t2.jpeg';
+import AVTR3 from '../../assets/t3.jpg';
+import AVTR4 from '../../assets/t4.jpeg';
+
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Sarah James',
+    review: ' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt rerum reiciendis autem natus quia delectus ullam facilis esse deleniti. Laborum',
+  },  {
+    avatar: AVTR2,
+    name: 'Mark Greg',
+    review: ' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt rerum reiciendis autem natus quia delectus ullam facilis esse deleniti. Laborum',
+  }, {
+    avatar: AVTR4,
+    name: 'Paul Cole',
+    review: ' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt rerum reiciendis autem natus quia delectus ullam facilis esse deleniti. Laborum',
+  }, {
+    avatar: AVTR3,
+    name: 'Rita Lee',
+    review: ' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt rerum reiciendis autem natus quia delectus ullam facilis esse deleniti. Laborum',
+  },
+]
+
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+  <h5>Review from clients</h5>
+  <h2>Testimonials</h2>
+
+  <Swiper  modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}   
+      className="container testimonials_container">
+    {
+      data.map(({avatar, name, review}, index)=>{
+        return (
+   <SwiperSlide key={index} className="testimonial">
+     <div className="client_avatar">
+      <img src={avatar} alt="" />
+     </div>  
+     <h5 className='client_name'>{name}</h5>
+      <small className='client_review'>
+      {review}  
+      </small>
+
+    </SwiperSlide>
+ 
+        )
+      })
+    }
+    
+     </Swiper>
+
+
+    </section>
   )
 }
 
